c9234262e65b4d2aab8f190eabc932a7ab64010b
jzc <justincai.lps@gmail.com>
2021-05-17 11:19:18 -0600

    in progress

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { ParametricGeometries } from "three/examples/jsm/geometries/ParametricGeometries.js";
+import { ShaderPass }     from "three/examples/jsm/postprocessing/ShaderPass.js";
+import { TexturePass }    from "three/examples/jsm/postprocessing/TexturePass.js";
+import { ClearPass }      from "three/examples/jsm/postprocessing/ClearPass.js";
+import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer.js";
 import * as dat from "dat.gui";
 
 // setup three.js
@@ -9,7 +13,7 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
     75, window.innerWidth/window.innerHeight, 0.1, 1000
 );
-const renderer = new THREE.WebGLRenderer({antialias: true});
+const renderer = new THREE.WebGLRenderer({antialias: false});
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setClearColor(new THREE.Color(.1, .1, .1));
 document.getElementById("container").appendChild(renderer.domElement);
@@ -36,7 +40,6 @@ for (let i = 0; i < apothem*2+1; i++) {
 }
 scene.add(grid);
 grid.position.z = -3;
-console.log(scene.children);
 
 // setup camo material
 const vs = `
@@ -61,6 +64,7 @@ varying vec2 vNDC;
 varying float vDistToCameraPlane;
 
 uniform sampler2D preCamo;
+uniform sampler2D preCamoDepth;
 uniform float displacementScale;
 uniform float blend;
 
@@ -69,11 +73,25 @@ void main(void) {
     vec2 displacement = displacementScale / vDistToCameraPlane * -1.0 * projection;
     vec2 v = 0.5*(vNDC + vec2(1.0));
     vec4 normalColor = vec4(vec3(0.5) + vNormalMS*0.5, 1.0);
+    float depth = gl_FragCoord.z;
+    if (texture2D(preCamoDepth, v+displacement).x < depth) { 
+     // gl_FragDepth = 0.0; 
+     gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+     // discard;
+     gl_FragColor = mix(texture2D(preCamo, v), normalColor, blend);
+    } else {
     gl_FragColor = mix(texture2D(preCamo, v+displacement), normalColor, blend);
+    // gl_FragColor = vec4(texture2D(preCamoDepth, v), 1.0);
+    // gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0); 
+    }
 }
 `;
 
-const preCamoTarget = new THREE.WebGLRenderTarget(1080, 720);
+
+
+const preCamoTarget = new THREE.WebGLRenderTarget(window.innerWidth, window.innerHeight);
+preCamoTarget.depthTexture = new THREE.DepthTexture(window.innerWidth, window.innerHeight);
+preCamoTarget.depthTexture.minFilter = THREE.NearestFilter;
 const camoMaterial = new THREE.ShaderMaterial({
     vertexShader: vs,
     fragmentShader: fs,
@@ -81,6 +99,7 @@ const camoMaterial = new THREE.ShaderMaterial({
 	displacementScale: { value: .1 },
 	blend: { value: 0 },
 	preCamo: { value: preCamoTarget.texture },
+	preCamoDepth: { value: preCamoTarget.depthTexture },
     }
 });
 
@@ -115,6 +134,30 @@ const mobius = new THREE.Mesh(
 addCamoObjectToScene(mobius);
 mobius.position.x = -7;
 
+const smoothFragmentShader = `
+uniform sampler2D tDiffuse;
+
+void main() {
+ int s = 5;
+ vec4 sum = vec4(0.0, 0.0, 0.0, 0.0);
+ for (int i = -s; i <= s; i++) {
+  for (int j = -s; j <= s; j++) {
+   sum += texture2D(tDiffuse, gl_FragCoord.xy + vec2(i, j));
+  }
+ }
+ gl_FragColor = sum / 9.0;
+ // gl_FragColor = vec4(0.0, 0.0, 1.0, 1.0);
+}
+`
+
+const composerRt = new THREE.WebGLRenderTarget(window.innerWidth, window.innerHeight);
+const composer = new EffectComposer(renderer, composerRt);
+composer.renderToScreen = false;
+composer.addPass(new ClearPass());
+composer.addPass(new TexturePass(preCamoTarget.texture));
+composer.addPass(new ShaderPass(
+    {fragmentShader: smoothFragmentShader}
+));
 // loop
 
 const state_machine = {
@@ -163,10 +206,25 @@ let gui = new dat.GUI();
 let config = {};
 gui.add(state_machine, "toggleCamo");
 
+const orthoCamera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0, 1);
+const square = new THREE.Mesh(
+    new THREE.PlaneGeometry(.5, .5),
+    new THREE.MeshBasicMaterial({map: composerRt.texture})
+)
+const orthoScene = new THREE.Scene();
+// orthoScene.add(square);
+// square.position.x = -0.875;
+// square.position.y = 0.875;
+renderer.autoClear = false;
+
+// console.log(composer.);
+
 const controls = new OrbitControls(camera, renderer.domElement);
 function animate(t) {
     requestAnimationFrame( animate );
     controls.update();
+
+    
 
     state_machine.t = t;
     const state = state_machine.state;
@@ -174,8 +232,11 @@ function animate(t) {
 
     camera.layers.disable(1);
     renderer.setRenderTarget(preCamoTarget);
+    renderer.clear();
     renderer.render(scene, camera);
     renderer.setRenderTarget(null);
+
+    composer.render();
 
     camera.layers.enable(1);
     for (let i = 0; i < hasCamo.length; i++) {
@@ -183,7 +244,9 @@ function animate(t) {
 	hasCamo[i].rotation.y += .02;
     }
 
+    renderer.clear();
     renderer.render(scene, camera);
+    renderer.render(orthoScene, orthoCamera);
 }
 
 animate();
